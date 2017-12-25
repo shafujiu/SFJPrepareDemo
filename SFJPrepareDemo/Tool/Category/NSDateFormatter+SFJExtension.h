abda78d232647052bed3e7e47f055138fb4de42e
赵维 <>
2017-12-25 17:12:25 +0800

    格式化日期

--- SFJPrepareDemo/Tool/Category/NSDateFormatter+SFJExtension.h
+++ SFJPrepareDemo/Tool/Category/NSDateFormatter+SFJExtension.h
@@ -29,4 +29,30 @@
 // 5. dateStr to DateStr
 + (NSString *)sfj_dateStrToDateStr:(NSString *)dateStr fromFormatterStr:(NSString *)fFormatter toFormatterStr:(NSString *)toformatter;
 
+
+/**
+ *  计算上次日期距离现在多久
+ *
+ *  @param lastTime    上次日期(需要和格式对应)
+ *  @param format1     上次日期格式
+ *  @param currentTime 最近日期(需要和格式对应)
+ *  @param format2     最近日期格式
+ *
+ *  @return xx分钟前、xx小时前、xx天前
+ */
++ (NSString *)sfj_timeIntervalFromLastTime:(NSString *)lastTime
+                        lastTimeFormat:(NSString *)format1
+                         ToCurrentTime:(NSString *)currentTime
+                     currentTimeFormat:(NSString *)format2;
+
+/**
+ 计算上次的时间距离现在多久
+
+ @param lastTime 上一次的date
+ @param currentTime 这一次的date
+ @return xx分钟前、xx小时前、xx天前
+ */
++ (NSString *)sfj_timeIntervalFromLastDate:(NSDate *)lastTime
+                             ToCurrentDate:(NSDate *)currentTime;
+
 @end
